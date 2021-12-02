8bbe987d939d0999f032ab57ffbad5ad59c67c2f
GU Junhua <jhgu@nao.cas.cn>
2021-12-02 21:30:48 +0800

    modified:   Cargo.toml 	modified:   src/bin/observe_white_noise.rs 	modified:   src/main.rs

--- src/bin/observe_white_noise.rs
+++ src/bin/observe_white_noise.rs
@@ -1,139 +1,170 @@
-use rustfft::{
-    FftPlanner
+use std::{
+    fs::{File, OpenOptions},
+    io::Write,
+    path::Path,
 };
+
+use serde_yaml::{from_reader, to_writer};
+
+use rustfft::FftPlanner;
 
 use rand::{
-    Rng
-    ,SeedableRng
-    //, thread_rng
+    Rng,
+    SeedableRng, //, thread_rng
 };
 
-use rand_chacha::{
-    ChaCha8Rng
-};
+use rand_chacha::ChaCha8Rng;
 
-use rand_distr::{
-    StandardNormal
-};
+use rand_distr::StandardNormal;
 
 use ndarray::{
-    Array2
-    , Axis
-    , s
-    //, parallel::prelude::*
+    s, //, parallel::prelude::*
+    Array2,
+    Axis,
 };
 
-use ndarray_npy::{
-    write_npy
-};
+//use ndarray_npy::write_npy;
 
-use num::{
-    complex::{
-        Complex
-    }
-};
+use num::complex::Complex;
 
-use clap::{
-    App
-    , Arg
-};
+use clap::{App, Arg};
 
-use pinknoise::{
-    VmPinkRng
-};
-
+use pinknoise::VmPinkRng;
 
 fn main() {
-    let matches=App::new("observe 1/f noise")
+    let matches = App::new("observe 1/f noise")
         .arg(
             Arg::new("nch")
-            .short('c')
-            .long("nch")
-            .takes_value(true)
-            .value_name("number of channels")
-            .required(true)
-            .about("number of channels of fft")
+                .short('c')
+                .long("nch")
+                .takes_value(true)
+                .value_name("number of channels")
+                .required(true)
+                .about("number of channels of fft"),
         )
         .arg(
             Arg::new("ncum")
-            .short('m')
-            .long("ncum")
-            .takes_value(true)
-            .value_name("number of cummulation")
-            .required(true)
-            .about("number of fft per point")
+                .short('m')
+                .long("ncum")
+                .takes_value(true)
+                .value_name("number of cummulation")
+                .required(true)
+                .about("number of fft per point"),
         )
         .arg(
             Arg::new("npt")
-            .short('n')
-            .long("npt")
-            .takes_value(true)
-            .value_name("number of points")
-            .required(true)
-            .about("number of points")
+                .short('n')
+                .long("npt")
+                .takes_value(true)
+                .value_name("number of points")
+                .required(true)
+                .about("number of points"),
+        )
+        .arg(
+            Arg::new("state_file")
+                .short('s')
+                .long("state")
+                .takes_value(true)
+                .value_name("state file")
+                .required(false)
+                .default_value("state.yaml")
+                .about("pink noise generator state"),
         )
         .arg(
             Arg::new("pink_rng_order")
-            .short('p')
-            .long("po")
-            .takes_value(true)
-            .value_name("order")
-            .required(false)
-            .default_value("48")
-            .about("pink noise generator order")
+                .short('p')
+                .long("po")
+                .takes_value(true)
+                .value_name("order")
+                .required(false)
+                .default_value("48")
+                .about("pink noise generator order"),
         )
         .arg(
             Arg::new("gain_std")
-            .short('g')
-            .long("gs")
-            .takes_value(true)
-            .value_name("gain std")
-            .required(false)
-            .default_value("0.01")
-            .about("gain std in dB")
+                .short('g')
+                .long("gs")
+                .takes_value(true)
+                .value_name("gain std")
+                .required(false)
+                .default_value("0.01")
+                .about("gain std in dB"),
         )
         .arg(
             Arg::new("out")
-            .short('o')
-            .long("out")
-            .takes_value(true)
-            .value_name("out name")
-            .required(true)
-            .about("output file")
+                .short('o')
+                .long("out")
+                .takes_value(true)
+                .value_name("out name")
+                .required(true)
+                .about("output file"),
         )
         .get_matches();
 
-    let nch=matches.value_of("nch").unwrap().parse::<usize>().unwrap();
-    let ncum=matches.value_of("ncum").unwrap().parse::<usize>().unwrap();
-    let outname=matches.value_of("out").unwrap();
-    let npt=matches.value_of("npt").unwrap().parse::<usize>().unwrap();
-    let pno=matches.value_of("pink_rng_order").unwrap().parse::<usize>().unwrap();
-    let gs=matches.value_of("gain_std").unwrap().parse::<f64>().unwrap();
+    let nch = matches.value_of("nch").unwrap().parse::<usize>().unwrap();
+    let ncum = matches.value_of("ncum").unwrap().parse::<usize>().unwrap();
+    let outname = matches.value_of("out").unwrap();
+    let npt = matches.value_of("npt").unwrap().parse::<usize>().unwrap();
+    let pno = matches
+        .value_of("pink_rng_order")
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let gs = matches
+        .value_of("gain_std")
+        .unwrap()
+        .parse::<f64>()
+        .unwrap();
+    let state_fname = matches.value_of("state_file").unwrap();
 
-    let mut planner=FftPlanner::<f64>::new();
-    let fft=planner.plan_fft_forward(nch*2);
-    let mut buffer=Array2::<Complex<f64>>::zeros((ncum, nch*2));
+    let mut planner = FftPlanner::<f64>::new();
+    let fft = planner.plan_fft_forward(nch * 2);
+    let mut buffer = Array2::<Complex<f64>>::zeros((ncum, nch * 2));
 
-    let mut rng=ChaCha8Rng::from_entropy();
+    let mut rng = ChaCha8Rng::from_entropy();
 
-    let mut vmpn=VmPinkRng::<f64>::new(pno, &mut rng);
+    let mut vmpn = if Path::new(state_fname).exists() {
+        println!("read pink noise state from {}", state_fname);
+        let mut state_file = File::open(state_fname).unwrap();
+        from_reader(&mut state_file).unwrap()
+    } else {
+        VmPinkRng::<f64>::new(pno, &mut rng)
+    };
 
-    let mut output=Array2::zeros((npt, nch));
-    
-    for (i, mut row) in output.axis_iter_mut(Axis(0)).enumerate(){
+    for i in 0..npt {
         {
-            println!("{}", i as f64/ npt as f64);
+            println!("{}", i as f64 / npt as f64);
         }
-        buffer.iter_mut().for_each(|x|{
-            let gain=10_f64.powf(vmpn.get(&mut rng)*gs/10.0);
-            let signal:f64=rng.sample(StandardNormal);
-            *x=(signal*gain).into();
+        buffer.iter_mut().for_each(|x| {
+            let gain = 10_f64.powf(vmpn.get(&mut rng) * gs / 10.0);
+            let signal: f64 = rng.sample(StandardNormal);
+            *x = (signal * gain).into();
         });
-        buffer.axis_iter_mut(Axis(0)).for_each(|mut row|{
+        buffer.axis_iter_mut(Axis(0)).for_each(|mut row| {
             fft.process(row.as_slice_mut().unwrap());
         });
-        let outbuf=buffer.slice(s![.., ..nch]).map(|x| x.norm_sqr()).mean_axis(Axis(0)).unwrap();
-        row.assign(&outbuf);
+        let outbuf = buffer
+            .slice(s![.., ..nch])
+            .map(|x| x.norm_sqr())
+            .mean_axis(Axis(0))
+            .unwrap();
+
+        let mut state_file = File::create(state_fname).unwrap();
+        to_writer(&mut state_file, &vmpn).unwrap();
+
+        let mut outfile = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .write(true)
+            .open(outname)
+            .unwrap();
+        outfile
+            .write(unsafe {
+                std::slice::from_raw_parts(
+                    outbuf.as_slice().unwrap().as_ptr() as *const u8,
+                    outbuf.len() * std::mem::size_of::<f64>(),
+                )
+            })
+            .unwrap();
     }
-    write_npy(outname, &output).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,93 @@
-use rustfft::{
-    FftPlanner
-};
+use rustfft::FftPlanner;
 
-use rand::{
-    SeedableRng
-    //, thread_rng
-};
+use rand::SeedableRng;
 
-use rand_chacha::{
-    ChaCha8Rng
-};
+use rand_chacha::ChaCha8Rng;
 
 use ndarray::{
-    Array2
-    , Axis
-    , s
-    //, parallel::prelude::*
+    s, //, parallel::prelude::*
+    Array2,
+    Axis,
 };
 
-use ndarray_npy::{
-    write_npy
-};
+use ndarray_npy::write_npy;
 
-use num::{
-    complex::{
-        Complex
-    }
-};
+use num::complex::Complex;
 
-use clap::{
-    App
-    , Arg
-};
+use clap::{App, Arg};
 
-use pinknoise::{
-    VmPinkRng
-};
-
+use pinknoise::VmPinkRng;
 
 fn main() {
-    let matches=App::new("observe 1/f noise")
+    let matches = App::new("observe 1/f noise")
         .arg(
             Arg::new("nch")
-            .short('c')
-            .long("nch")
-            .takes_value(true)
-            .value_name("number of channels")
-            .required(true)
-            .about("number of channels of fft")
+                .short('c')
+                .long("nch")
+                .takes_value(true)
+                .value_name("number of channels")
+                .required(true)
+                .about("number of channels of fft"),
         )
         .arg(
             Arg::new("ncum")
-            .short('m')
-            .long("ncum")
-            .takes_value(true)
-            .value_name("number of cummulation")
-            .required(true)
-            .about("number of fft per point")
+                .short('m')
+                .long("ncum")
+                .takes_value(true)
+                .value_name("number of cummulation")
+                .required(true)
+                .about("number of fft per point"),
         )
         .arg(
             Arg::new("npt")
-            .short('n')
-            .long("npt")
-            .takes_value(true)
-            .value_name("number of points")
-            .required(true)
-            .about("number of points")
+                .short('n')
+                .long("npt")
+                .takes_value(true)
+                .value_name("number of points")
+                .required(true)
+                .about("number of points"),
         )
         .arg(
             Arg::new("out")
-            .short('o')
-            .long("out")
-            .takes_value(true)
-            .value_name("out name")
-            .required(true)
-            .about("output file")
+                .short('o')
+                .long("out")
+                .takes_value(true)
+                .value_name("out name")
+                .required(true)
+                .about("output file"),
         )
         .get_matches();
 
-    let nch=matches.value_of("nch").unwrap().parse::<usize>().unwrap();
-    let ncum=matches.value_of("ncum").unwrap().parse::<usize>().unwrap();
-    let outname=matches.value_of("out").unwrap();
-    let npt=matches.value_of("npt").unwrap().parse::<usize>().unwrap();
+    let nch = matches.value_of("nch").unwrap().parse::<usize>().unwrap();
+    let ncum = matches.value_of("ncum").unwrap().parse::<usize>().unwrap();
+    let outname = matches.value_of("out").unwrap();
+    let npt = matches.value_of("npt").unwrap().parse::<usize>().unwrap();
 
-    let mut planner=FftPlanner::<f64>::new();
-    let fft=planner.plan_fft_forward(nch*2);
-    let mut buffer=Array2::<Complex<f64>>::zeros((ncum, nch*2));
+    let mut planner = FftPlanner::<f64>::new();
+    let fft = planner.plan_fft_forward(nch * 2);
+    let mut buffer = Array2::<Complex<f64>>::zeros((ncum, nch * 2));
 
-    let mut rng=ChaCha8Rng::from_entropy();
+    let mut rng = ChaCha8Rng::from_entropy();
 
-    let mut vmpn=VmPinkRng::<f64>::new(48, &mut rng);
+    let mut vmpn = VmPinkRng::<f64>::new(48, &mut rng);
 
-    let mut output=Array2::zeros((npt, nch));
-    
-    for (i, mut row) in output.axis_iter_mut(Axis(0)).enumerate(){
+    let mut output = Array2::zeros((npt, nch));
+
+    for (i, mut row) in output.axis_iter_mut(Axis(0)).enumerate() {
         {
-            println!("{}", i as f64/ npt as f64);
+            println!("{}", i as f64 / npt as f64);
         }
-        buffer.iter_mut().for_each(|x|{
-            *x=vmpn.get(&mut rng).into();
+        buffer.iter_mut().for_each(|x| {
+            *x = vmpn.get(&mut rng).into();
         });
-        buffer.axis_iter_mut(Axis(0)).for_each(|mut row|{
+        buffer.axis_iter_mut(Axis(0)).for_each(|mut row| {
             fft.process(row.as_slice_mut().unwrap());
         });
-        let outbuf=buffer.slice(s![.., ..nch]).map(|x| x.norm_sqr()).mean_axis(Axis(0)).unwrap();
+        let outbuf = buffer
+            .slice(s![.., ..nch])
+            .map(|x| x.norm_sqr())
+            .mean_axis(Axis(0))
+            .unwrap();
         row.assign(&outbuf);
     }
     write_npy(outname, &output).unwrap();
